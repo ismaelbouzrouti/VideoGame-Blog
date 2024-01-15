fb62d25006629a6b6b5225ad0f46dadcb1d5a95c
ismaelbouzrouti <132908223+ismaelbouzrouti@users.noreply.github.com>
2024-01-15 21:07:53 +0100

    perf(aboutpage): hover added

--- resources/views/about-page.blade.php
+++ resources/views/about-page.blade.php
@@ -13,46 +13,56 @@
                 <li class="mb-4">
                     Laravel Documentation:
                     <ul class="list-inside">
-                        <li><a href="https://laravel.com/docs" class="text-blue-500" target="_blank">Official Laravel
-                                Docs</a></li>
-                        <li><a href="https://laracasts.com" class="text-blue-500" target="_blank">Laracasts</a></li>
-                        <li><a href="https://www.tutorialspoint.com/laravel/index.htm" class="text-blue-500"
-                                target="_blank">Tutorialspoint Laravel</a></li>
-                        <li><a href="https://www.stackhawk.com/blog/laravel-xss" class="text-blue-500"
+                        <li><a href="https://laravel.com/docs" class="text-blue-500 hover:underline"
+                                target="_blank">Official Laravel Docs</a></li>
+                        <li><a href="https://laracasts.com" class="text-blue-500 hover:underline"
+                                target="_blank">Laracasts</a></li>
+                        <li><a href="https://www.tutorialspoint.com/laravel/index.htm"
+                                class="text-blue-500 hover:underline" target="_blank">Tutorialspoint Laravel</a></li>
+                        <li><a href="https://www.stackhawk.com/blog/laravel-xss" class="text-blue-500 hover:underline"
                                 target="_blank">Laravel XSS Guide</a></li>
                         <li><a href="https://www.stackhawk.com/blog/laravel-csrf-protection-guide/"
-                                class="text-blue-500" target="_blank">Laravel CSRF Protection Guide</a></li>
+                                class="text-blue-500 hover:underline" target="_blank">Laravel CSRF Protection Guide</a>
+                        </li>
                         <li><a href="https://dev.to/robinkashyap_01/a-beginners-guide-to-resource-routes-in-laravel-4d2j"
-                                class="text-blue-500" target="_blank">Resource Routes in Laravel</a></li>
-                        <li><a href="https://www.cloudways.com/blog/laravel-migrations/" class="text-blue-500"
-                                target="_blank">Laravel Migrations</a></li>
+                                class="text-blue-500 hover:underline" target="_blank">Resource Routes in Laravel</a>
+                        </li>
+                        <li><a href="https://www.cloudways.com/blog/laravel-migrations/"
+                                class="text-blue-500 hover:underline" target="_blank">Laravel Migrations</a></li>
                         <li><a href="https://www.tutorialspoint.com/laravel/laravel_controllers.htm"
-                                class="text-blue-500" target="_blank">Laravel Controllers</a></li>
+                                class="text-blue-500 hover:underline" target="_blank">Laravel Controllers</a></li>
                     </ul>
                 </li>
                 <li class="mb-4">
                     Tailwind CSS Documentation:
                     <ul class="list-inside">
-                        <li><a href="https://tailwindcss.com/docs" class="text-blue-500" target="_blank">Official
-                                Tailwind CSS Docs</a></li>
+                        <li><a href="https://tailwindcss.com/docs" class="text-blue-500 hover:underline"
+                                target="_blank">Official Tailwind CSS Docs</a></li>
                     </ul>
                 </li>
                 <li class="mb-4">
                     PHP Documentation:
                     <ul class="list-inside">
-                        <li><a href="https://www.php.net/docs.php" class="text-blue-500" target="_blank">Official PHP
-                                Docs</a></li>
-                        <li><a href="https://www.tutorialspoint.com/php/index.htm" class="text-blue-500"
+                        <li><a href="https://www.php.net/docs.php" class="text-blue-500 hover:underline"
+                                target="_blank">Official PHP Docs</a></li>
+                        <li><a href="https://www.tutorialspoint.com/php/index.htm" class="text-blue-500 hover:underline"
                                 target="_blank">Tutorialspoint PHP</a></li>
                     </ul>
                 </li>
-                <li>
+                <li class="mb-4">
                     Debugging & Errors:
                     <ul class="list-inside">
-                        <li><a href="https://stackoverflow.com/" class="text-blue-500" target="_blank">Stack
-                                Overflow</a></li>
-                        <li><a href="https://chat.openai.com/" class="text-blue-500" target="_blank">OpenAI ChatGPT</a>
-                        </li>
+                        <li><a href="https://stackoverflow.com/" class="text-blue-500 hover:underline"
+                                target="_blank">Stack Overflow</a></li>
+                        <li><a href="https://chat.openai.com/" class="text-blue-500 hover:underline"
+                                target="_blank">OpenAI ChatGPT</a></li>
+                    </ul>
+                </li>
+                <li class="mb-4">
+                    GitHub Repo:
+                    <ul class="list-inside">
+                        <li><a href="https://github.com/ismaelbouzrouti/VideoGame-Blog"
+                                class="text-blue-500 hover:underline" target="_blank">VideoGameBlog Repo</a></li>
                     </ul>
                 </li>
             </ul>
